62bbabd60537dee80d670f9f72555e86e0eb4e14
kipelovets <kipelovets@mail.ru>
2015-04-04 01:33:21 +0300

    + mysql role

--- roles/mysql/templates/mysql_dump.sh
+++ roles/mysql/templates/mysql_dump.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-USER={{ mysql_db_password }}
+USER={{ mysql_db_user }}
 PASSWORD={{ mysql_db_password }}
 BACKUP_PATH="{{ backup_dir }}"
 DATE=$(date +"%Y-%m-%d")
+TARGET_FILE=$BACKUP_PATH/mysqldump-$DATE.sql
 
-mysqldump --user=$USER --password=$PASSWORD --all-databases > $BACKUP_PATH/mysqldump-$DATE.sql 
+echo "Writing dump to $TARGET_FILE\n"
+mysqldump --user=$USER --password=$PASSWORD --all-databases > "$TARGET_FILE" 
+gzip -v -f $TARGET_FILE
