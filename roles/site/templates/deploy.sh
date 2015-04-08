dc1ed53008c1826901a5bebe56e2464605c88099
kipelovets <kipelovets@mail.ru>
2015-04-09 01:45:44 +0300

    deploy finished

--- roles/site/templates/deploy.sh
+++ roles/site/templates/deploy.sh
@@ -8,9 +8,11 @@ flock -x -w 10 200 || exit 1
 DIR="{{ code_dir }}"
 WORKS_DIR="/var/data/{{ domain }}/works"
 IMAGES_DIR="/var/data/{{ domain }}/images"
+LOGS_DIR="/var/log/{{ domain }}"
 
 if [ "$#" -ne 5 ]; then
     echo "Illegal number of parameters"
+    exit 1
 fi
 
 REPO=$1
@@ -25,18 +27,23 @@ DATE=`date +"%FT%T"`
 TARGET_DIR="$DIR/$DATE-$COMMIT"
 
 git clone -b $BRANCH $URL $TARGET_DIR
-rm -rf "$TARGET_DIR/.git"
+LOGS_DIR_LINK="$TARGET_DIR/app/logs"
+rm -rf "$TARGET_DIR/.git" $LOGS_DIR_LINK
 
-WORK_DIR_LINK="$TARGET_DIR/public_html/works"
-IMAGES_DIR_LINK="$TARGET_DIR/public_html/images"
-ln -s $WORKS_DIR $WORK_DIR_LINK 
-ln -s $IMAGES_DIR $IMAGES_DIR_LINK 
+ln -s $WORKS_DIR "$TARGET_DIR/public_html/works" 
+ln -s $IMAGES_DIR "$TARGET_DIR/public_html/images" 
 ln -s "$DIR/parameters.yml" "$TARGET_DIR/app/config/parameters.yml"
+ln -s $LOGS_DIR $LOGS_DIR_LINK
 
 cd $TARGET_DIR
 /usr/bin/composer install -o
 app/console cache:warmup
+app/console assetic:dump --env=prod --no-debug
+app/console assets:install --symlink public_html
 
-ln -fs "$TARGET_DIR" "$DIR/current" 
+chown -R www-data:www-data $LOGS_DIR_LINK
+chown -R www-data:www-data app/cache
+
+ln -sf "$TARGET_DIR" "$DIR/current" 
 
 ) 200>/var/lock/{{ domain }}.lock
