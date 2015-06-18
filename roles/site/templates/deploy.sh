d8d45dfe3aedb456edaecf79d19ea2d4cab5d5c2
kipelovets <kipelovets@mail.ru>
2015-06-19 00:24:40 +0300

    deploy script

--- roles/site/templates/deploy.sh
+++ roles/site/templates/deploy.sh
@@ -11,9 +11,9 @@ IMAGES_DIR="/var/data/{{ domain }}/images"
 LOGS_DIR="/var/log/{{ domain }}"
 
 if [ "$#" -ne 5 ]; then
-    echo "Illegal number of parameters"
+    echo "Usage: deploy.sh <git repo> <project> <branch> <type> <commit>"
     exit 1
-fi
+gi 
 
 REPO=$1
 #PROJECT=$2
@@ -21,7 +21,7 @@ BRANCH=$3
 #TYPE=$4
 COMMIT=$5
 
-URL="https://github.com/${1}"
+URL="https://github.com/$REPO"
 
 DATE=`date +"%FT%T"`
 TARGET_DIR="$DIR/$DATE-$COMMIT"
@@ -36,6 +36,7 @@ ln -s "$DIR/parameters.yml" "$TARGET_DIR/app/config/parameters.yml"
 ln -s $LOGS_DIR $LOGS_DIR_LINK
 
 cd $TARGET_DIR
+export HOME=/root
 /usr/bin/composer install -o
 app/console cache:warmup
 app/console assetic:dump --env=prod --no-debug
